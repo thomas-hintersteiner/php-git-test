f70f12e0306bbc609894e0f62bd06fa0496a7da7
thomas_hintersteiner <thomas.hintersteiner@enjoyit.at>
2024-05-21 13:22:07 +0200

    added Lastnames

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
     // names that should be displayed
     $names = ['Hansi Hinterseer', 'Susi Musterfrau', 'Max Mustermann', 'Jane Doe', 'John Doe'];
+
+    $lastNames = ['Maier', 'Müller', 'Mutz'];
 ?>
 
 <!DOCTYPE html>
@@ -11,8 +13,16 @@
     <title>PHP Testprojekt</title>
 </head>
 <body>
+    <h1>PHP Testprojekt</h1>
+
+    <h2>Names</h2>
     <?php foreach($names as $name): ?>
         <p><?= $name ?></p>
+    <?php endforeach; ?>
+
+    <h2>Lastnames</h2>
+    <?php foreach($lastNames as $lastName): ?>
+        <p><?= $lastName ?></p>
     <?php endforeach; ?>
 </body>
 </html>
