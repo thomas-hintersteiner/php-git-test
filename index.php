2a5e4466cf2577a4c9f86dabc47c090c0b10cd46
thomas_hintersteiner <thomas.hintersteiner@enjoyit.at>
2024-05-21 12:59:45 +0200

    initial checkin

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    $names = ['Susi Musterfrau', 'Max Mustermann', 'Jane Doe'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Testprojekt</title>
+</head>
+<body>
+    <?php foreach($names as $name): ?>
+        <p><?= $name ?></p>
+    <?php endforeach; ?>
+</body>
+</html>
